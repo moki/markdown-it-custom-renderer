e60c5941133eed704494359a81e22ff10e23c176
moki <morozov.kirill.moki@gmail.com>
2023-04-09 22:57:43 +0400

    infra: bundle types

--- scripts/build.esm.js
+++ scripts/build.esm.js
@@ -1,5 +1,6 @@
 const esbuild = require('esbuild');
 const {nodeExternalsPlugin} = require('esbuild-node-externals');
+const {Generator} = require('npm-dts');
 
 const {dependencies = [], peerDependencies = []} = require('../package.json');
 
@@ -15,5 +16,11 @@ esbuild
         target: 'node14',
         external: [...Object.keys(dependencies), ...Object.keys(peerDependencies)],
         plugins: [nodeExternalsPlugin()],
+    })
+    .then(() => {
+        new Generator({
+            entry: 'src/index.ts',
+            output: 'lib/index.d.ts',
+        }).generate();
     })
     .catch(() => process.exit(1));
